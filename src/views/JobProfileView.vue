3e8192dbbfa31ad194c10ec793d75fa251518d1a
ormunoz <or.munozf@gmail.com>
2023-09-23 01:56:30 -0300

    se crea vista sin completar de perfil del trabajo

---
+++ src/views/JobProfileView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>job profile</h1>
+    </div>
+</template>
+<script>
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+    name: 'JobProfile',
+    setup(props) {
+
+    }
+})
+
+</script>
